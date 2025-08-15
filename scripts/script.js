1a1beebaf989a91203e4b17b990fe79440388ae2
Madu Jang <madu.jang93@gmail.com>
2025-08-15 01:28:39 +0100

    mobile design excluding creation images

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const creationImages = document.querySelectorAll('.creation img');
+
+window.addEventListener('resize', () => {
+  const screenSize = window.innerWidth;
+
+});
+
+console.log(creationImages)
