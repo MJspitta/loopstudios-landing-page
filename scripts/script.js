6a43ead00a21dcc2e3d33684507a7136453d8aa8
Madu Jang <madu.jang93@gmail.com>
2025-08-19 00:26:02 +0100

    responsive design and hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,39 @@
 const creationImages = document.querySelectorAll('.creation img');
 
-window.addEventListener('resize', () => {
-  const screenSize = window.innerWidth;
+if (window.innerWidth <= 480) {
+  creationImages.forEach((cimg) => {
+    const imgLink = cimg.src.replace("desktop", "mobile");
+    cimg.src = imgLink;
+  });
+} else {
+  creationImages.forEach((cimg) => {
+    const imgLink = cimg.src.replace("mobile", "desktop");
+    cimg.src = imgLink;
+  });
+}
 
+window.addEventListener('resize', (e) => {
+  if (e.target.innerWidth <= 480) {
+    creationImages.forEach((cimg) => {
+      const imgLink = cimg.src.replace("desktop", "mobile");
+      cimg.src = imgLink;
+    });
+  } else {
+    creationImages.forEach((cimg) => {
+      const imgLink = cimg.src.replace("mobile", "desktop");
+      cimg.src = imgLink;
+    });
+  }
 });
 
-console.log(creationImages)
+const hamburgerBtn = document.querySelector('.hamburger-menu img');
+const closeBtn = document.querySelector('.close-menu img');
+const dialog = document.querySelector('dialog');
+
+hamburgerBtn.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+});
